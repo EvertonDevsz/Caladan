374b90153ec8a7770723ddaab8ff075e5c1147e9
EvertonDevsz <154710084+EvertonDevsz@users.noreply.github.com>
2026-05-18 17:55:22 -0300

    Configurando javascript do carrosel

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,25 +1,36 @@
 // ==================== STATE ==================== //
-
-
-
-
-
-
-
-
-
-
-
-
-
+let currentNavIndex = 0
 
 // ==================== DOM ==================== //
 const mainNav = document.querySelector(".navegacao-principal")
+const navContainer = document.querySelector(".itens")
+const navTrack = navContainer.querySelector(".nav-track")
+const navItems = navTrack.querySelectorAll(".item-navegacao")
+
 // ==================== HELPERS ==================== //
 // ==================== RENDER ==================== //
+function modifyNavigation(){
+    const itemWidth = navItems[0].offsetWidth
+    const visibleItems = Math.round(navContainer.offsetWidth / itemWidth)
+    const maxIndex = navItems.length - visibleItems
+
+    currentNavIndex++
+
+    if(currentNavIndex > maxIndex){
+        currentNavIndex = 0
+    }
+
+    navTrack.style.transform = `translateX(-${currentNavIndex * itemWidth}px)`
+}
 // ==================== EVENTS ==================== //
 const burgerMenu = document.querySelector("#menu-hamburguer")
 burgerMenu.addEventListener("click", () => {
     mainNav.classList.toggle("escondido")
 })
+
+const nextButton = document.querySelector(".botao-de-proximo")
+nextButton.addEventListener("click", modifyNavigation)
+window.addEventListener("resize", modifyNavigation)
+
+
 
